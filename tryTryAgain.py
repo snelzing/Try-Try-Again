08b2c04db5f0bff8bc57f2ab8de0959a0ae2f645
snelzing <39928924+snelzing@users.noreply.github.com>
2019-02-08 15:11:07 -0500

    Add files via upload

---
+++ tryTryAgain.py
@@ -0,0 +1,8 @@
+# Trying to get a for-loop to allow the inputter to
+# let you keep trying over and over again
+# Nothing fancy
+
+cow = input("Type cow here \n")
+
+while cow != "cow":
+    cow = input("please try again, you absolute failure of a collection of proteins. \n")
